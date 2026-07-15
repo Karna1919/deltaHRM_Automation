25560992426cbe93e2efbdffe7c61df29c45d538
PratheepRaj-009 <pratheepkumar561@gmail.com>
2026-07-15 11:28:56 +0530

    smoke1

---
+++ tests/smoke/Search_Employee_in_PIM.spec.js
@@ -0,0 +1,13 @@
+import{test}from"../../Fixtures/loginFicture.js"
+/**
+       * @param {import('playwright').Page} page
+       */
+import HomePage from "../../delta_Page_Object_Model/homePage.js"
+
+test("User should able to search for an employee in the PIM Employee List",async({loginPage})=>
+{
+    let page=loginPage
+    let homePage= new HomePage(page)
+    await homePage.PIMLink.click()
+
+})
