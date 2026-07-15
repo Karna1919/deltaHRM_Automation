1f1a6e0df5527d416223a9f9c0aa7bd025f225be
srushhh86 <srushhh86@gmail.com>
2026-07-15 12:21:02 +0530

    edited TC_01

--- tests/smoke/TC_01.spec.js
+++ tests/smoke/TC_01.spec.js
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test"
 import Login from "../../delta_Page_Object_Model/loginPage"
 
-test("Login with Valid Credentials", async (params) => {
+test("Login with Valid Credentials", async ({page}) => {
 
     let loginPg = new Login(page)
     await loginPg.loginToApplication()
